428fc9b37738eb119ff93f06030bda01083bb5ed
b-sandrine <sanrdushimimana@gmail.com>
2023-06-16 08:28:30 +0200

    Added dasboard header

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,19 @@
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { FaHome } from "react-icons/fa";
+import profile from "../../assets/profile.png"
+import '../../assets/styles/dashboard.css'
+
 const Dashboard = () => {
     return (
         <div className="dashboard--container">
             <Sidebar />
+            <div className="content--header">
+                <div className="title">
+                    <FaHome />
+                    <p>Dashboard</p>
+                </div>
+                <img src={profile} alt="Profile Image" />
+            </div>
         </div>
     )
 }
