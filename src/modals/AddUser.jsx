166aaa6a8bda0643011939b245b612190832d945
b-sandrine <sanrdushimimana@gmail.com>
2023-06-16 14:12:04 +0200

    Added and integrated modal

--- src/modals/AddUser.jsx
+++ src/modals/AddUser.jsx
@@ -1,6 +1,37 @@
 import './AddUser.css'
 import '../assets/styles/main.css'
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import axios from 'axios';
+
 const Adduser = ({ isOpen, onClose }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState({
+        fullnames: "",
+        email: "",
+        NID: 0,
+        phoneNumber: "",
+        address: "",
+        username: "",
+        password: "",
+    })
+
+    function handleOnChange(e) {
+        e.preventDefault();
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    function handleNavigate() {
+        console.log(user)
+        axios.post('http://localhost:8080/api/create', user)
+        .then((response) => {
+            console.log(response)
+            navigate('/dashboard')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
     return (
         <div className={`modal ${isOpen ? 'modal-open' : ''}`}>
             <div className="modal-content">
@@ -9,16 +40,14 @@ const Adduser = ({ isOpen, onClose }) => {
                 </div>
                 <h2>Add New Car Owner</h2>
                 <div className="form">
-                    <input type="text" name="fullnames" id="" placeholder='Full Names' />
-                    <input type="email" name="email" id="" placeholder='Email' />
-                    <input type="number" name="NID" id="" placeholder='National Id' />
-                    <input type="text" name="phoneNumber" id="" placeholder='Phone number' />
-                    <input type="text" name="address" id="" placeholder='Address' />
-                    <input type="text" name="username" id="" placeholder='Username' />
-                    <input type="password" name="password" id="" placeholder='Password' />
+                    <input type="text" name="fullnames" id="" placeholder='Full Names' value={user.fullnames} onChange={handleOnChange} />
+                    <input type="email" name="email" id="" placeholder='Email' value={user.email} onChange={handleOnChange} />
+                    <input type="number" name="NID" id="" placeholder='National Id' value={user.NID} onChange={handleOnChange} />
+                    <input type="text" name="phoneNumber" id="" placeholder='Phone number' value={user.phoneNumber} onChange={handleOnChange} />
+                    <input type="text" name="address" id="" placeholder='Address' value={user.address} onChange={handleOnChange} />
                     <div className="buttons">
                         <input type="submit" name="" id="" value="Cancel" className='cancel' onClick={onClose} />
-                        <input type="submit" name="" id="" value="Submit" className='success' />
+                        <input type="submit" name="" id="" value="Submit" className='success' onClick={handleNavigate}/>
                     </div>
                 </div>
             </div>
